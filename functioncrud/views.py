b5aec398222e7952bd50aabcbb0ba60d986384c1
baaaam771 <jeongiun@naver.com>
2020-08-29 14:48:10 +0900

    function type CRUD (Create, Read)

--- functioncrud/views.py
+++ functioncrud/views.py
@@ -11,7 +11,20 @@ def read(request):
     return render(request, 'functioncrud/funccrud.html', {'blogs':blogs})
 
 def create(request):
-    return
+    #새로운 데이터 새로운 블로그 글 저장하는 역할 == POST
+    if request.method == 'POST':
+        #입력한 블로그 글들을 저장해라
+        form = NewBlog(request.POST)
+        if form.is_valid:
+            post = form.save(commit=False)
+            post.pub_date = timezone.now()
+            post.save()
+            return redirect('home')
+    #글쓰기 페이지를 띄워주는 역할 == GET(!POST)
+    else:
+        # 단순히 입력받을 수 있는 form을 띄워줘라
+        form =NewBlog()
+        return render(request, 'functioncrud/new.html', {'form':form})
 
 def update(request):
     return
